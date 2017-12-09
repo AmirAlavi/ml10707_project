6b73d68414ee8dffc034f24cec5d3bc3342114a0
Amir Alavi <aalavi@andrew.cmu.edu>
2017-12-08 23:01:12 -0500

    Finished Sparse Coding model code

--- sparse_coding.py
+++ sparse_coding.py
@@ -2,11 +2,12 @@
 import csv
 import datetime
 import pickle
-import string 
+import string
+import multiprocessing
 
 import numpy as np
 import pandas as pd
-from sklearn.decomposition import SparseCoder, DictionaryLearning
+from sklearn.decomposition import SparseCoder, DictionaryLearning, MiniBatchDictionaryLearning
 from scipy.spatial.distance import cdist
 
 
@@ -25,19 +26,21 @@ def get_only_alpha(df):
 
 def sample_from_dataframe(df, include_list, n=3000):
     # Take n words from the dataframe. Additionally, add the words in include_list
-    sample = df.sample(n=n)
+    #sample = df.sample(n=n)
+    sample = df.head(n)
     print(sample.shape)
     for word in include_list:
-        if word not in sample:
+        if word not in sample.index:
             print("adding " + word + " to dataframe")
             sample = sample.append(df.loc[word])
+            print(word in sample.index)
     return sample
 
 
 def load_word_embeddings(path='glove.6B.50d.txt'):
     df = pd.read_csv(path, sep=" ", header=None, index_col=0,
                      quoting=csv.QUOTE_NONE)
-    df.drop(df.index[14798], inplace=True) # Studid word is called "nan", causes bugs
+    df.drop(df.index[14798], inplace=True) # Stupid word is called "nan", causes bugs
     return df
 
 def save_dict(model, path="dictionary.p"):
@@ -50,8 +53,12 @@ def load_dict(path="dictionary.p"):
         return pickle.load(f)
 
 def learn_dictionary(X):
-    dictionary = DictionaryLearning(n_components=500, fit_algorithm='lars',
-                                    transform_n_nonzero_coefs=5, verbose=2)
+    num_cpus = multiprocessing.cpu_count()
+    print(num_cpus, " CPUs")
+    # dictionary = DictionaryLearning(n_components=2000, fit_algorithm='lars',
+    #                                 transform_n_nonzero_coefs=5, verbose=2, n_jobs=num_cpus)
+    dictionary = MiniBatchDictionaryLearning(n_components=2000, fit_algorithm='lars', transform_algorithm='lars',
+                                             transform_n_nonzero_coefs=5, verbose=2, n_jobs=num_cpus, batch_size=16, n_iter=1000)
     t0 = datetime.datetime.now()
     dictionary.fit(X)
     t1 = datetime.datetime.now()
@@ -76,36 +83,44 @@ def get_atoms(model, word_embedding):
     activated_atoms = model.components_[activated_atoms_filter]
     return activated_atoms
 
-def analyze_atoms_of_discourse(model_file, embeddings_file, words=["tie", "star", "bank", "cut", "bass", "connecticut"]):
+def analyze_atoms_of_discourse(model_file, embeddings_file, words=["bank", "blue", "credit", "fly", "light", "mean", "star", "tie", "abrogate", "ceramics", "connecticut", "imperfect", "sleepless", "spoken"]):
     model = load_dict(model_file)
     word_embeddings = pd.read_pickle(embeddings_file)
     for word in words:
-        print("Looking at word: ", word)
+        print("\n\n\nLooking at word: ", word)
         word_embedding = word_embeddings.loc[word].values
         atoms = get_atoms(model, word_embedding)
         find_nearest_words(atoms, word_embeddings)
         
 
 
-if __name__ == "__main__":
-    analyze_atoms_of_discourse("results/glove_10000_n500_k3/dictionary_10000_n500_k3.p", "results/glove_10000_n500_k3/selected_df_10k.p")
+# Uncomment for analyzing the learned model
 
+if __name__ == "__main__":
+    #analyze_atoms_of_discourse("results/glove_10000_n500_k3/dictionary_10000_n500_k3.p", "results/glove_10000_n500_k3/selected_df_10k.p", words=["tie", "star", "bank", "cut", "bass", "connecticut"])
+    #analyze_atoms_of_discourse("results/glove_100000_n500_k3_lars/dictionary_100000_n500_k3.p", "results/glove_100000_n500_k3_lars/selected_df_100k_k3.p", words=["tie", "star", "bank", "cut", "bass", "connecticut"])
+    #analyze_atoms_of_discourse("results/glove_100000_n500_k3/dictionary_100000_n500_k3.p", "results/glove_100000_n500_k3/selected_df_100k_k3.p")
+    analyze_atoms_of_discourse("results/glove_100000_n2000_k5_lars/dictionary_100000_n2000_k5.p", "results/glove_100000_n2000_k5/selected_df_100k_k5.p", words=["bank", "blue", "credit", "fly", "light", "mean", "star", "tie", "abrogate", "ceramics", "connecticut", "imperfect", "sleepless", "spoken", "cut", "bass"])
+
+
+
+# Uncomment for learning the Sparse Coding Model
 
 # if __name__ == "__main__":
 #     df = load_word_embeddings()
 #     filtered = get_only_alpha(df)
 #     print(filtered.shape)
-#     selected_df = sample_from_dataframe(filtered, include_list=["tie", "star", "bank", "cut", "bass", "connecticut"], n=10000)
+#     selected_df = sample_from_dataframe(filtered, include_list=["bank", "blue", "credit", "fly", "light", "mean", "star", "tie", "abrogate", "ceramics", "connecticut", "imperfect", "sleepless", "spoken", "cut", "bass"], n=100000)
 #     print(selected_df.shape)
 #     # Save the dataframe for Ruochi:
-#     selected_df.to_pickle("selected_df_10k_k5.p")
-#     selected_df.to_csv("selected_df_10k_k5.csv", header=False)
+#     selected_df.to_pickle("selected_df_100k_k5.p")
+#     selected_df.to_csv("selected_df_100k_k5.csv", header=False)
 
 #     X = selected_df.as_matrix()
 #     dictionary = learn_dictionary(X)
-#     save_dict(dictionary, "dictionary_10000_n500_k5.p")
+#     save_dict(dictionary, "dictionary_100000_n2000_k5.p")
 
-#     dictionary = load_dict("dictionary_10000_n500_k5.p")
+#     dictionary = load_dict("dictionary_100000_n2000_k5.p")
 #     basis_vectors = dictionary.components_
 #     print(basis_vectors.shape)
 
@@ -114,3 +129,4 @@ if __name__ == "__main__":
 #     # 5000 words, 500 components, k=3, Dictionary learning took  2 hours 12 mins 44 secs, 1000 iterations, final cost: 34718.476
 #     # 10000 words, 500 components, k=3, Dictionary learning took  5 hours  0 mins 56 secs, 1000 iterations, final cost: 71434.546
 #     # 10000 words, 500 components, k=5, Dictionary learning took  5 hours  8 mins 57 secs, 1000 iterations, final cost: 71518.951
+#     # 100000 words, 2000 components, k=5, Dictionary learning took  0 hours 25 mins 44 secs, 1000 iterations
